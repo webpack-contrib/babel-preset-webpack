380c5780b720959ccd123992d07079c1ec55e632
Joshua Wiens <joshuaw@easymetrics.com>
2017-01-27 00:28:53 -0600

    feat(babel): add proposal plugins

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 module.exports = {
-  presets: [
-    require('babel-preset-es2015-without-strict')
-  ],
   plugins: [
-    [require('babel-plugin-transform-es2015-template-literals'), { spec: true }],
-    require('babel-plugin-transform-es3-member-expression-literals'),
-    require('babel-plugin-transform-es3-property-literals'),
-    require('babel-plugin-transform-jscript'),
-    require('babel-plugin-transform-exponentiation-operator'),
+    require("babel-plugin-check-es2015-constants"),
     require('babel-plugin-syntax-trailing-function-commas'),
-    [require('babel-plugin-transform-object-rest-spread'), { useBuiltIns: true }],
+    require("babel-plugin-transform-es2015-arrow-functions"),
+    require("babel-plugin-transform-es2015-block-scoped-functions"),
+    require("babel-plugin-transform-es2015-block-scoping"),
+    require("babel-plugin-transform-es2015-classes"),
+    require("babel-plugin-transform-es2015-computed-properties"),
+    require("babel-plugin-transform-es2015-destructuring"),
+    require("babel-plugin-transform-es2015-duplicate-keys"),
+    require("babel-plugin-transform-es2015-for-of"),
+    require("babel-plugin-transform-es2015-function-name"),
+    require("babel-plugin-transform-es2015-literals"),
+    [require("babel-plugin-transform-es2015-modules-commonjs"), {strict : false}],
+    require("babel-plugin-transform-es2015-object-super"),
+    require("babel-plugin-transform-es2015-parameters"),
+    require("babel-plugin-transform-es2015-shorthand-properties"),
+    require("babel-plugin-transform-es2015-spread"),
+    require("babel-plugin-transform-es2015-sticky-regex"),
+    [require('babel-plugin-transform-es2015-template-literals'), { spec: true }],
+    require("babel-plugin-transform-es2015-typeof-symbol"),
+    require("babel-plugin-transform-es2015-unicode-regex"),
+    require('babel-plugin-transform-exponentiation-operator'),
+    [require('babel-plugin-transform-object-rest-spread'), { useBuiltIns: true }]
   ],
 };
